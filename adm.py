6c8001309eedf80bff926c81c91bab9c6c304d7f
Anton Jedrzejewski <207309941+maant1s@users.noreply.github.com>
2025-11-03 15:18:30 +0100

    moved db_host parameter to .env instead of hardcoding

--- adm.py
+++ adm.py
@@ -5,11 +5,12 @@ from os import getenv
 load_dotenv()   # loads enviromental variables
 
 DB_NAME = getenv("DB_NAME")
+DB_HOST = getenv("DB_HOST")
 SQL_USER = getenv("SQL_USER")
 SQL_PASSWORD = getenv("SQL_PASSWORD")
 
 def getDatabaseConnection():
-	return psycopg.connect(f"dbname={DB_NAME} user={SQL_USER} password={SQL_PASSWORD} host='sxterm'")
+	return psycopg.connect(f"dbname={DB_NAME} host={DB_HOST} user={SQL_USER} password={SQL_PASSWORD}")
 
 conn = getDatabaseConnection()
 cur = conn.cursor()
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from os import getenv
 load_dotenv()   # loads enviromental variables
 
 DB_NAME = getenv("DB_NAME")
+DB_HOST = getenv("DB_HOST")
 SQL_USER = getenv("SQL_USER")
 SQL_PASSWORD = getenv("SQL_PASSWORD")
 
-with pies.connect(f"dbname={DB_NAME} user={SQL_USER} password={SQL_PASSWORD} host='sxterm'") as conn:
+with pies.connect(f"dbname={DB_NAME} host={DB_HOST} user={SQL_USER} password={SQL_PASSWORD}") as conn:
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM kategoria;")
         for record in cur:
