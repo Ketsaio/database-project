634a2e10bd3f585b14ef992081254b057a401909
Anton Jedrzejewski <207309941+maant1s@users.noreply.github.com>
2025-11-03 12:28:06 +0100

    added host to connection string

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ DB_NAME = getenv("DB_NAME")
 SQL_USER = getenv("SQL_USER")
 SQL_PASSWORD = getenv("SQL_PASSWORD")
 
-with pies.connect(f"dbname={DB_NAME} user={SQL_USER} password={SQL_PASSWORD}") as conn:
+with pies.connect(f"dbname={DB_NAME} user={SQL_USER} password={SQL_PASSWORD} host='sxterm'") as conn:
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM transport;")
+        cur.execute("SELECT * FROM kategoria;")
         for record in cur:
             print(record)
