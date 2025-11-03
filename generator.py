26922f3d469abf5a092926e618729c599022357b
Ketsaio <bartosz.kaczorowski@poczta.fm>
2025-11-03 14:16:14 +0100

    Add generator for clients, orders and order details

---
+++ generator.py
@@ -0,0 +1,67 @@
+import psycopg as ps
+from os import getenv
+from dotenv import load_dotenv
+from faker import Faker
+from random import randint, choice
+
+load_dotenv()
+fake = Faker("pl_PL")
+
+DB_NAME = getenv("DB_NAME")
+SQL_USER = getenv("SQL_USER")
+SQL_PASSWORD = getenv("SQL_PASSWORD")
+
+with ps.connect(f"dbname={DB_NAME} user={SQL_USER} password={SQL_PASSWORD} host='sxterm'") as conn:
+    with conn.cursor() as cur:
+        client_ids = []
+        for _ in range(5):
+            client_name = fake.first_name()
+            client_surname = fake.last_name()
+            is18 = False
+            if randint(0,99) >= 18:
+                is18 = True
+            login = client_name + "." + client_surname + ".mch"
+            password = fake.password()
+            mail = fake.email()
+            telephone = fake.phone_number()[:15]
+            cur.execute("INSERT INTO klient (imie, nazwisko, czy_18plus, login, haslo, email, telefon) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id_klienta", (client_name, client_surname, is18, login, password, mail, telephone))
+            client_ids.append(cur.fetchone()[0])
+
+        orders_ids = []
+        STATUS = ["OCZEKUJĄCE", "WYSŁANE", "DOSTARCZONE"]
+
+        for _ in range(5):
+            client_id = choice(client_ids)
+            date = fake.date_between(start_date="-1w")
+            status = choice(STATUS)
+            cost = 0.0
+            cur.execute("INSERT INTO zamowienia (id_klienta, data, status, kwota) VALUES (%s, %s, %s, %s) RETURNING id_zam", (client_id, date, status, cost))
+            orders_ids.append(cur.fetchone()[0])
+
+        for _ in range(5):
+            order_id = choice(orders_ids)
+            id_prod = 1
+            count = randint(1,10)
+            cur.execute("SELECT cena FROM produkt WHERE id_prod = 1;")
+            this_cost = cur.fetchone()[0]
+            this_cost *= count
+            cur.execute("INSERT INTO szczegolyzam (id_zam, id_prod, ilosc, cena) VALUEs (%s, %s, %s, %s)", (order_id, id_prod, count, this_cost))
+
+
+        cur.execute("SELECT * FROM szczegolyzam;")
+
+        x = cur.fetchall()
+        cos = {}
+        for i in range(len(x)):
+            if x[i][1] not in cos:
+                cos[x[i][1]] = x[i][4]
+            else:
+                cos[x[i][1]] += x[i][4]
+
+        print(cos)
+
+        for k, v in cos.items():
+            cur.execute(f"UPDATE zamowienia SET kwota = {float(v)} WHERE id_zam = {k}")
+
+
+
