d7fe87b8d7acd15f64e98e44e2c56e392f16803f
Anton Jedrzejewski <207309941+maant1s@users.noreply.github.com>
2025-11-05 20:48:57 +0100

    added modifyProduct, seems to work fine

--- adm.py
+++ adm.py
@@ -1,6 +1,7 @@
 import psycopg
 from dotenv import load_dotenv
 from os import getenv
+from typing import Optional, cast, LiteralString
 
 load_dotenv()   # loads enviromental variables
 
@@ -104,14 +105,67 @@ def addNewCompany():
 	cur.execute("INSERT INTO producent (nazwa, opis, id_kat) VALUES (%s, %s, %s);", list(fields.values()))
 	conn.commit()
 
+def modifyProduct():
+	rows = []
+	
+	while len(rows) != 1:
+		product = input("Enter product ID or name: ").strip()
+		if product.isdigit():
+			cur.execute("SELECT id_prod, nazwa FROM produkt WHERE id_prod = %s;", (int(product),))
+		else:
+			cur.execute("SELECT id_prod, nazwa FROM produkt WHERE LOWER(nazwa) LIKE %s;", (f"%{product.lower()}%",))
+		rows = cur.fetchall()
+
+		if len(rows) == 0:
+			print("Cannot find product with given ID or name.")
+		elif len(rows) > 1:
+			print("Found multiple matches:")
+			for row in rows:
+				print(row)
+			print("Please refine your search.")
+
+	prodId = rows[0][0]
+	print(rows[0])
+	opt = input(f"selected {rows[0][1]}, [e]dit / [d]elete? ")
+
+	if opt == 'd' or opt == 'D':
+		cur.execute("DELETE FROM produkt WHERE id_prod = %s;", (prodId,))
+		conn.commit()
+		return
+	
+	fields: dict[str, Optional[str]] = {
+		"opis" : None,
+		"cena" : None,
+		"stan_rzeczywisty" : None,
+		"stan_wirtualny" : None,
+	}
+	
+	updateQuery = "UPDATE produkt SET "
+	updateValues = []
+	for it in fields.keys():
+		newValue = input(f"new {it} (leave empty to keep current): ")
+		if newValue != "":
+			updateQuery += f"{it} = %s, "
+			updateValues.append(newValue)
+	
+
+	updateQuery = updateQuery.rstrip(", ") + " WHERE id_prod = %s;"
+	updateValues.append(prodId)
+
+	cur.execute(cast(LiteralString, updateQuery), updateValues)
+	conn.commit()
 
 while True:
-	opt = int(input("1. new product / 2. new category / 3. new company "))
+	opt = int(input("1. new product / 2. new category / 3. new company / 4. modify product / 5. exit: "))
 	if opt == 1:
 		addNewProduct()
 	elif opt == 2:
 		addNewCategory()
 	elif opt == 3:
 		addNewCompany()
+	elif opt == 4:
+		modifyProduct()
+	elif opt == 5:
+		break
 	else:
 		print("invalid option\n\n")
