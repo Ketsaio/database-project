62784fe4e074ead7788a8bf452ef301922d3aee9
Anton Jedrzejewski <207309941+maant1s@users.noreply.github.com>
2025-11-07 22:11:39 +0100

    all functionality works, todo code cleanup and output formatting

--- adm.py
+++ adm.py
@@ -124,12 +124,11 @@ def modifyProduct():
 				print(row)
 			print("Please refine your search.")
 
-	prodId = rows[0][0]
 	print(rows[0])
 	opt = input(f"selected {rows[0][1]}, [e]dit / [d]elete? ")
 
 	if opt == 'd' or opt == 'D':
-		cur.execute("DELETE FROM produkt WHERE id_prod = %s;", (prodId,))
+		cur.execute("DELETE FROM produkt WHERE id_prod = %s;", (rows[0][0],))
 		conn.commit()
 		return
 	
@@ -150,13 +149,79 @@ def modifyProduct():
 	
 
 	updateQuery = updateQuery.rstrip(", ") + " WHERE id_prod = %s;"
-	updateValues.append(prodId)
+	updateValues.append(rows[0][0])
 
 	cur.execute(cast(LiteralString, updateQuery), updateValues)
 	conn.commit()
 
-while True:
-	opt = int(input("1. new product / 2. new category / 3. new company / 4. modify product / 5. exit: "))
+def getAllOrders():
+	limit = input("number of recent orders to fetch (default 20): ")
+	if limit.isdigit() is False:
+		limit = 20
+
+	cur.execute("SELECT * FROM zamowienia ORDER BY id_zam DESC LIMIT %s;", (int(limit),))	
+		
+	for row in cur.fetchall():
+		print(row)
+
+def getUnrealizedOrders():
+	cur.execute("SELECT * FROM zamowienia WHERE status LIKE 'niezrealizowane' ORDER BY id_zam;")
+		
+	for row in cur.fetchall():
+		print(row)
+
+def getOrderDetails():
+	orderId = input("Enter order ID: ")
+	if orderId.isdigit() is False:
+		print("invalid order ID")
+		return
+	
+	cur.execute("SELECT * FROM szczegolyzam WHERE id_zam = %s;", (orderId,))
+
+	if cur.rowcount == 0:
+		print("no details found for given order ID")
+		return
+		
+	for row in cur.fetchall():
+		print(row)
+
+	cur.execute("SELECT * FROM zamowienia WHERE id_zam = %s;", (orderId,))
+	print(cur.fetchone())
+
+	opt = input("[m]ark as realized / [c]ancel: ")
+	if opt == 'm' or opt == 'M':
+		cur.execute("UPDATE zamowienia SET status = 'zrealizowane' WHERE id_zam = %s;", (orderId,))
+		conn.commit()
+		print("order marked as realized")
+
+	elif opt == 'c' or opt == 'C':
+		cur.execute("DELETE FROM szczegolyzam WHERE id_zam = %s;", (orderId,))
+		cur.execute("DELETE FROM zamowienia WHERE id_zam = %s;", (orderId,))
+		conn.commit()
+		print("order canceled")
+
+def checkAvailability():
+	productId = input("Enter product ID or leave blank for all: ")
+
+	if productId != "":
+		if productId.isdigit() is False:
+			print("invalid product ID")
+			return
+		
+		cur.execute("SELECT nazwa, stan_rzeczywisty, stan_wirtualny FROM produkt WHERE id_prod = %s;", (int(productId),))
+		row = cur.fetchone()
+		if row is None:
+			print("no product found with given ID")
+		else:
+			print(row)
+	
+	else:
+		cur.execute("SELECT nazwa, stan_rzeczywisty, stan_wirtualny FROM produkt;")
+		for row in cur.fetchall():
+			print(row)
+
+def addingMenu():
+	opt = int(input("1. new product / 2. new category / 3. new company / 4. back: "))
 	if opt == 1:
 		addNewProduct()
 	elif opt == 2:
@@ -164,8 +229,43 @@ while True:
 	elif opt == 3:
 		addNewCompany()
 	elif opt == 4:
-		modifyProduct()
-	elif opt == 5:
-		break
+		return
 	else:
 		print("invalid option\n\n")
+
+def ordersMenu():
+	opt = int(input("1. get all orders / 2. get unrealized orders / 3. get order details / 4. back: "))
+	if opt == 1:
+		getAllOrders()
+	elif opt == 2:
+		getUnrealizedOrders()
+	elif opt == 3:
+		getOrderDetails()
+	elif opt == 4:
+		return
+	else:
+		print("invalid option\n\n")
+
+
+def mainMenu():
+	while True:
+		opt = int(input("1. add new / 2. modify product / 3. check availability / 4. orders / 5. exit: "))
+		if opt == 1:
+			addingMenu()
+		elif opt == 2:
+			modifyProduct()
+		elif opt == 3:
+			checkAvailability()
+		elif opt == 4:
+			ordersMenu()
+		elif opt == 5:
+			print("exiting...")
+			break
+		else:
+			print("invalid option\n\n")
+
+	cur.close()
+	conn.close()
+
+if __name__ == "__main__":
+	mainMenu()
